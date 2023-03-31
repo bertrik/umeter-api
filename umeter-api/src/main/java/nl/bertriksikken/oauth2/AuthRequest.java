b55b1d41271d230686f66bc5f0f1a5914e21db0c
Bertrik Sikken <bertrik@sikken.nl>
2023-03-31 18:00:12 +0200

    Remove guava dependency.

--- umeter-api/src/main/java/nl/bertriksikken/oauth2/AuthRequest.java
+++ umeter-api/src/main/java/nl/bertriksikken/oauth2/AuthRequest.java
@@ -1,12 +1,7 @@
 package nl.bertriksikken.oauth2;
 
-import java.net.URLEncoder;
 import java.util.LinkedHashMap;
 import java.util.Map;
-import java.util.stream.Collectors;
-
-import com.google.common.base.Charsets;
-import com.google.common.collect.ImmutableMap;
 
 public final class AuthRequest {
 
@@ -23,14 +18,9 @@ public final class AuthRequest {
         request.values.put("password", password);
         return request;
     }
-
-    public String encode() {
-        return values.keySet().stream().map(k -> k + "=" + URLEncoder.encode(values.get(k), Charsets.UTF_8))
-                .collect(Collectors.joining("&"));
-    }
     
     public Map<String, String> map() {
-        return ImmutableMap.copyOf(values);
+        return Map.copyOf(values);
     }
 
 }
